5a56b8563f112daea0d3a1e1a7346573275e0621
alenitycruz <alessandradsmcruz@recife.pe.senac.br>
2022-02-13 22:42:31 -0300

    Exercicio de revisao sobre metodo com sobrecarga

---
+++ src/br/com/revisao/Main.java
@@ -0,0 +1,13 @@
+package br.com.revisao;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //resultado
+        Quadrilatero.area(2,2);
+        Quadrilatero.area(5,6,3);
+        Quadrilatero.area(5);
+    }
+}
